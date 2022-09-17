c4d552e5c5b1132dc387d9f1ac34533fbcd4e5b8
Hiroshi Nagata <HIROSN@LIVE.COM>
2022-09-17 13:08:05 -0700

    day_2_part_2

---
+++ 2021/day_2_part_2.cpp
@@ -0,0 +1,29 @@
+#include "main.h"
+
+int Answer(std::ifstream &file)
+{
+    std::string command;
+    int units = 0;
+    int position = 0;
+    int depth = 0;
+    int aim = 0;
+
+    while (file >> command >> units)
+    {
+        if (command == "forward")
+        {
+            position += units;
+            depth += aim * units;
+        }
+        else if (command == "down")
+        {
+            aim += units;
+        }
+        else if (command == "up")
+        {
+            aim -= units;
+        }
+    }
+
+    return position * depth;
+}
