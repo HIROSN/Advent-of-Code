5aabc41a53b7ddefede00c43e388d649b00f8553
Hiroshi Nagata <HIROSN@LIVE.COM>
2022-12-06 15:30:25 -0800

    Day 6: Tuning Trouble

---
+++ 2022/day_6_part_2.cpp
@@ -0,0 +1,37 @@
+#include <main.h>
+
+#include <map>
+
+std::optional<uint64_t> Answer(std::ifstream &file)
+{
+    const int marker_size = 14;
+    std::string line;
+
+    while (std::getline(file, line))
+    {
+        std::map<char, int> characters;
+
+        for (int i = 0; i < line.size(); i++)
+        {
+            characters[line[i]]++;
+
+            if (i < marker_size)
+            {
+                continue;
+            }
+
+            if (--characters[line[i - marker_size]] == 0)
+            {
+                characters.erase(line[i - marker_size]);
+            }
+
+            if (characters.size() == marker_size)
+            {
+                std::cout << (i + 1) << std::endl;
+                break;
+            }
+        }
+    }
+
+    return {};
+}
