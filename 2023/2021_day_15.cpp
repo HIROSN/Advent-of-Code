d8fbbd592fbe224fd329eaedbc1a0d76bff3fb25
Hiroshi Nagata <HIROSN@LIVE.COM>
2023-12-23 15:58:53 -0800

    Day 15: Chiton

---
+++ 2023/2021_day_15.cpp
@@ -0,0 +1,93 @@
+// #include <debug_print.h>
+#include <main.h>
+#include <traveler.h>
+
+#include <climits>
+#include <functional>
+#include <map>
+#include <queue>
+#include <utility>
+#include <vector>
+
+std::optional<uint64_t> Answer(std::ifstream &file)
+{
+    std::vector<std::string> cavern;
+    std::string line;
+
+    while (file >> line)
+        cavern.push_back(line);
+
+    int size_x = line.size();
+    int size_y = cavern.size();
+    DPRINT(cavern);
+
+    std::vector<std::vector<int>> risks;
+    std::vector<std::vector<bool>> entered;
+
+    for (int y = 0; y < size_y; y++)
+    {
+        risks.emplace_back(size_x, INT_MAX);
+        entered.emplace_back(size_x, false);
+    }
+
+    using AdjacentPointsQueueType =
+        std::priority_queue<std::pair<int, aoc::Traveler>,
+                            std::vector<std::pair<int, aoc::Traveler>>,
+                            std::greater<std::pair<int, aoc::Traveler>>>;
+
+    AdjacentPointsQueueType entered_points;
+    std::map<aoc::Traveler, bool> queued;
+    aoc::Traveler traveler({0, 0}, {size_x - 1, size_y - 1});
+    risks[0][0] = 0;
+    entered[0][0] = true;
+
+    auto risk_at = [&](int x, int y) -> int
+    {
+        return cavern[y][x] - '0';
+    };
+
+    while (true)
+    {
+        const int &x = traveler.x;
+        const int &y = traveler.y;
+
+        for (const auto &offset : traveler.direction.offsets())
+        {
+            int ax = x + offset.x;
+            int ay = y + offset.y;
+
+            if (traveler.valid(offset) && !entered[ay][ax])
+            {
+                if (risks[ay][ax] > risks[y][x] + risk_at(ax, ay))
+                {
+                    risks[ay][ax] = risks[y][x] + risk_at(ax, ay);
+                }
+
+                auto traveler_copy = traveler;
+                traveler_copy.visit(offset);
+
+                if (queued.find(traveler_copy) == queued.end())
+                {
+                    entered_points.push({risks[ay][ax], traveler_copy});
+                    queued[traveler_copy] = true;
+                }
+            }
+        }
+
+        if (traveler.is_at_end())
+            DPRINT3(traveler.get_path(), '.', 1);
+
+        if (!entered_points.empty())
+        {
+            traveler = entered_points.top().second;
+            entered_points.pop();
+            queued.erase(traveler);
+            entered[y][x] = true;
+            continue;
+        }
+        break;
+    }
+
+    // DPRINT(risks);
+    return risks[size_y - 1][size_x - 1];
+}
---
+++ include/traveler.h
@@ -0,0 +1,431 @@
+#pragma once
+
+/*
+// ForwardFour
+//
+aoc::Traveler traveler({0, 0}, {100, 100});
+DPRINTX_ENDL(traveler);
+for (const auto &offset : traveler.direction.offsets())
+{
+    traveler.visit(offset);
+    DPRINTX_ENDL(traveler);
+}
+
+{0,0}
+{0,0}>{1,0}
+{0,0}>{1,0}v{1,1}
+{0,0}>{1,0}v{1,1}^{1,0}
+{0,0}>{1,0}v{1,1}^{1,0}<{0,0}
+
+// StraightFirst
+//
+aoc::Traveler traveler({0, 0}, {100, 100});
+DPRINTX_ENDL(traveler);
+for (int i = 0; i < 4; i++)
+{
+    traveler.visit(traveler.direction.offsets()[0]);
+    DPRINTX_ENDL(traveler);
+}
+
+{0,0}
+{0,0}>{1,0}
+{0,0}>{1,0}>{2,0}
+{0,0}>{1,0}>{2,0}>{3,0}
+{0,0}>{1,0}>{2,0}>{3,0}>{4,0}
+
+// BackwardFour
+//
+aoc::Traveler traveler({0, 0}, {100, 100});
+traveler.direction.neighbors = aoc::BackwardFour();
+DPRINTX_ENDL(traveler);
+for (const auto &offset : traveler.direction.offsets())
+{
+    traveler.visit(offset);
+    DPRINTX_ENDL(traveler);
+}
+
+{0,0}
+{0,0}<{-1,0}
+{0,0}<{-1,0}^{-1,-1}
+{0,0}<{-1,0}^{-1,-1}v{-1,0}
+{0,0}<{-1,0}^{-1,-1}v{-1,0}>{0,0}
+
+// TurnFirst
+//
+aoc::Traveler traveler({0, 0}, {100, 100});
+traveler.direction.type = aoc::Direction::Type::TurnFirst;
+DPRINTX_ENDL(traveler);
+for (const auto &offset : traveler.direction.offsets())
+{
+    traveler.visit(offset);
+    DPRINTX_ENDL(traveler);
+}
+
+{0,0}
+{0,0}v{0,1}
+{0,0}v{0,1}^{0,0}
+{0,0}v{0,1}^{0,0}>{1,0}
+{0,0}v{0,1}^{0,0}>{1,0}<{0,0}
+
+// Priority::Near
+//
+aoc::Point::priority = aoc::Point::Priority::Near;
+aoc::Traveler traveler({0, 0}, {size_x - 1, size_y - 1});
+
+std::priority_queue<std::pair<int, aoc::Traveler>,
+                    std::vector<std::pair<int, aoc::Traveler>>,
+                    std::less<std::pair<int, aoc::Traveler>>>;
++----------+
+| .^>>>.^>>|
+|v>>..v>>.v|
+|.........v|
+|......<<<v|
+|......v...|
+|...<<<v...|
+|<<<v...^>>|
+|v......^.v|
+|v.^>>.^>.v|
+|v>>.v>>..v|
++----------+
+215
+
+// Priority::Far
+//
+aoc::Traveler traveler({0, 0}, {size_x - 1, size_y - 1});
+
+std::priority_queue<std::pair<int, aoc::Traveler>,
+                    std::vector<std::pair<int, aoc::Traveler>>,
+                    std::greater<std::pair<int, aoc::Traveler>>>;
++----------+
+| .........|
+|v.........|
+|v>>>>>>...|
+|......v>..|
+|.......v..|
+|.......v>.|
+|........v.|
+|........v.|
+|........v>|
+|.........v|
++----------+
+40
+*/
+
+#include <map>
+#include <utility>
+#include <vector>
+
+#define Number int
+
+namespace aoc
+{
+    struct Point
+    {
+        Number x = 0;
+        Number y = 0;
+
+        Point() = default;
+        Point(Number px, Number py) : x(px), y(py) {}
+
+        enum Priority
+        {
+            Far,
+            Near,
+        };
+
+        virtual bool operator<(const Point &right) const
+        {
+            switch (priority)
+            {
+            case Priority::Far:
+                return x < right.x || (!(right.x < x) && y < right.y);
+            case Priority::Near:
+                return x > right.x || (!(right.x > x) && y > right.y);
+            }
+        }
+
+        virtual bool operator==(const Point &right) const
+        {
+            return x == right.x && y == right.y;
+        }
+
+        virtual bool operator>(const Point &right) const
+        {
+            return !(*this < right || *this == right);
+        }
+
+        virtual bool operator!=(const Point &right) const
+        {
+            return !(*this == right);
+        }
+
+        static Priority priority;
+
+        friend std::ostream &operator<<(
+            std::ostream &out, const Point &point);
+    };
+
+    Point::Priority Point::priority = Point::Priority::Far;
+
+    struct Offset : public Point
+    {
+        char ch = 0;
+
+        Offset() = default;
+        Offset(Number ox, Number oy, char oc) : Point(ox, oy), ch(oc) {}
+
+        virtual bool operator==(const Offset &right) const
+        {
+            return ch == right.ch;
+        }
+
+        virtual bool operator==(char right) const
+        {
+            return ch == right;
+        }
+
+        static Offset Right;
+        static Offset Down;
+        static Offset Left;
+        static Offset Up;
+        static Offset Stay;
+
+        friend std::ostream &operator<<(
+            std::ostream &out, const Offset &offset);
+    };
+
+    Offset Offset::Right = {1, 0, '>'};
+    Offset Offset::Down = {0, 1, 'v'};
+    Offset Offset::Left = {-1, 0, '<'};
+    Offset Offset::Up = {0, -1, '^'};
+    Offset Offset::Stay = {0, 0, 0};
+
+    struct Neighbors
+    {
+        static int number_of_offsets;
+        static std::vector<Offset> offsets;
+
+        friend std::ostream &operator<<(
+            std::ostream &out, const Neighbors &neighbors);
+    };
+
+    int Neighbors::number_of_offsets = 0;
+    std::vector<Offset> Neighbors::offsets;
+
+    struct FourNeighbors : public Neighbors
+    {
+        FourNeighbors()
+        {
+            number_of_offsets = 4;
+        }
+    };
+
+    struct ForwardFour : public FourNeighbors
+    {
+        ForwardFour() : FourNeighbors()
+        {
+            offsets = {Offset::Right, Offset::Down, Offset::Left, Offset::Up};
+        }
+    };
+
+    struct BackwardFour : public FourNeighbors
+    {
+        BackwardFour() : FourNeighbors()
+        {
+            offsets = {Offset::Left, Offset::Up, Offset::Right, Offset::Down};
+        }
+    };
+
+    struct Direction
+    {
+        Offset heading = Offset::Stay;
+
+        enum Type
+        {
+            StraightFirst,
+            TurnFirst,
+        };
+
+        Direction()
+        {
+            type = Type::StraightFirst;
+            neighbors = ForwardFour();
+        }
+
+        virtual int neighbor_index() const
+        {
+            for (int i = 0; i < neighbors.number_of_offsets; i++)
+                if (heading == neighbors.offsets[i])
+                    return i;
+            return 0;
+        }
+
+        virtual std::vector<Offset> offsets()
+        {
+            std::vector<Offset> offsets;
+            const int index_straight = neighbor_index();
+
+            if (index_straight < neighbors.number_of_offsets)
+            {
+                auto fix = [&](int index) -> int
+                {
+                    int size = neighbors.number_of_offsets;
+                    return (index + size) % size;
+                };
+
+                const int index_turn_right = fix(index_straight + 1);
+                const int index_turn_left = fix(index_straight - 1);
+
+                switch (type)
+                {
+                case Type::StraightFirst:
+                    offsets.push_back(neighbors.offsets[index_straight]);
+                    offsets.push_back(neighbors.offsets[index_turn_right]);
+                    offsets.push_back(neighbors.offsets[index_turn_left]);
+                    break;
+                case Type::TurnFirst:
+                    offsets.push_back(neighbors.offsets[index_turn_right]);
+                    offsets.push_back(neighbors.offsets[index_turn_left]);
+                    offsets.push_back(neighbors.offsets[index_straight]);
+                    break;
+                }
+
+                for (int i = 0; i < neighbors.number_of_offsets; i++)
+                {
+                    if (i != index_straight &&
+                        i != index_turn_right &&
+                        i != index_turn_left)
+                    {
+                        offsets.push_back(neighbors.offsets[i]);
+                    }
+                }
+            }
+            return offsets;
+        }
+
+        static Type type;
+        static Neighbors neighbors;
+    };
+
+    Direction::Type Direction::type = Type::StraightFirst;
+    Neighbors Direction::neighbors = ForwardFour();
+
+    struct Traveler
+    {
+        Number x = 0;
+        Number y = 0;
+        Direction direction;
+        std::vector<std::pair<Offset, Point>> visited;
+
+        Traveler() = default;
+
+        Traveler(const Point &point_start, const Point &point_end)
+        {
+            start = point_start;
+            end = point_end;
+            visited.push_back({direction.heading, start});
+        }
+
+        virtual bool valid(const Offset &offset) const
+        {
+            const auto ax = x + offset.x;
+            const auto ay = y + offset.y;
+            const auto size_x = end.x - start.x + 1;
+            const auto size_y = end.y - start.y + 1;
+            return ax >= 0 && ax < size_x &&
+                   ay >= 0 && ay < size_y;
+        }
+
+        virtual void visit(const Offset &offset)
+        {
+            x += offset.x;
+            y += offset.y;
+            direction.heading = offset;
+            visited.push_back({offset, {x, y}});
+        }
+
+        virtual void visit(Number ax, Number ay)
+        {
+            x = ax;
+            y = ay;
+        }
+
+        virtual bool is_at_end() const
+        {
+            return Point(x, y) == end;
+        }
+
+        virtual std::map<std::pair<int, int>, char> get_path(char ch = 0) const
+        {
+            std::map<std::pair<int, int>, char> path;
+            for (auto it = visited.rbegin(); it != visited.rend(); it++)
+                path[{it->second.x, it->second.y}] =
+                    ch ? ch : (it->first.ch ? it->first.ch : ' ');
+            return path;
+        }
+
+        virtual bool operator<(const Traveler &right) const
+        {
+            return Point(x, y) < Point(right.x, right.y);
+        }
+
+        virtual bool operator==(const Traveler &right) const
+        {
+            return Point(x, y) == Point(right.x, right.y);
+        }
+
+        virtual bool operator>(const Traveler &right) const
+        {
+            return !(*this < right || *this == right);
+        }
+
+        virtual bool operator!=(const Traveler &right) const
+        {
+            return !(*this == right);
+        }
+
+        static Point start;
+        static Point end;
+
+        friend std::ostream &operator<<(
+            std::ostream &out, const aoc::Traveler &traveler);
+    };
+
+    Point Traveler::start = {};
+    Point Traveler::end = {};
+
+    std::ostream &operator<<(std::ostream &out, const Point &point)
+    {
+#ifdef DPRINT_ON
+        out << "{" << point.x << "," << point.y << "}";
+#endif
+        return out;
+    }
+
+    std::ostream &operator<<(std::ostream &out, const Offset &offset)
+    {
+#ifdef DPRINT_ON
+        out << "{" << offset.ch << ":" << offset.x << "," << offset.y << "}";
+#endif
+        return out;
+    }
+
+    std::ostream &operator<<(std::ostream &out, const Neighbors &neighbors)
+    {
+#ifdef DPRINT_ON
+        for (const auto &offset : neighbors.offsets)
+            out << offset;
+#endif
+        return out;
+    }
+
+    std::ostream &operator<<(std::ostream &out, const Traveler &traveler)
+    {
+#ifdef DPRINT_ON
+        for (const auto &visit : traveler.visited)
+            out << visit.first.ch << visit.second;
+#endif
+        return out;
+    }
+
+} // namespace aoc
